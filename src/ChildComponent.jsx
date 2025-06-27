193a2baeb26a7d962bbf1e254cd41750d89c5857
Akshyy31 <akshayshaji688@gmail.com>
2025-06-27 17:34:19 +0530

    27-06-2025 first commit change

--- src/ChildComponent.jsx
+++ src/ChildComponent.jsx
@@ -3,18 +3,22 @@ import { MyContext } from "./UseContextTask";
 import Card from "react-bootstrap/Card";
 
 function ChildComponent() {
-    
   const users = useContext(MyContext);
   console.log(users);
 
   return (
     <div>
       <div className="cards-div">
+         
         {users
           ? users.map((user) => (
               <Card className="cards" style={{ width: "21rem" }}>
+                <Card.Title>User {user.id}</Card.Title>
                 <Card.Body>
-                  <Card.Text className="text"> Name : {user.firstName} {user.lastName} </Card.Text>
+                  <Card.Text className="text">
+                    {" "}
+                    Name : {user.firstName} {user.lastName}{" "}
+                  </Card.Text>
 
                   <Card.Text className="text"> Email : {user.email} </Card.Text>
                   <Card.Text className="text"> Age : {user.age} </Card.Text>
